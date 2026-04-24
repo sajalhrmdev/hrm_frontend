b88e1de8138ba505452298bb1f307f9087f9dd71
DebaprasadM <debaprasadmaiti2000@gmail.com>
2026-04-24 12:01:40 +0530

    type get problem solved

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -1,20 +1,22 @@
-// import { NextResponse } from "next/server";
-// import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
-// export async function GET() {
-//   const token =  cookies().get("token")?.value;
+export async function GET() {
+  const cookieStore = await cookies(); 
 
-//   if (!token) {
-//     return NextResponse.json({ user: null }, { status: 401 });
-//   }
+  const token = cookieStore.get("token")?.value;
 
-//   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/me`, {
-//     headers: {
-//       Authorization: `Bearer ${token}`,
-//     },
-//   });
+  if (!token) {
+    return NextResponse.json({ user: null }, { status: 401 });
+  }
 
-//   const data = await res.json();
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/me`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
-//   return NextResponse.json(data);
-// }
+  const data = await res.json();
+
+  return NextResponse.json(data);
+}
