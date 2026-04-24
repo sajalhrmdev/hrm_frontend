49956062ac167380643a5b04049d36f03003fdc7
DebaprasadM <debaprasadmaiti2000@gmail.com>
2026-04-24 11:31:59 +0530

    import type problem solved

--- src/components/uiInterface/advanced-ui/custom-declaration.d.ts
+++ src/components/uiInterface/advanced-ui/custom-declaration.d.ts
@@ -1,3 +1,5 @@
 declare module 'react-awesome-stars-rating';
 declare module '@react-latest-ui/react-sticky-notes';
 declare module 'bootstrap/dist/js/bootstrap.bundle.min.js';
+declare module '*.scss';
+declare module '*.css';
